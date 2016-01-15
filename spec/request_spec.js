fbbb806dcd55f3c148c361a664f2f2e4bfd2718f
Alexander Antonov <alexandr-post@yandex.ru>
2016-01-16 01:28:49 +0300

    jasmine-node test is under construction

--- spec/request_spec.js
+++ spec/request_spec.js
@@ -1,57 +1,50 @@
-var request = require('request');
-var url = require('url');
-
-var express = require('express'),
-    app = express();
-
 var HOST = 'localhost',
     PORT = 1337;
 
+var request = require('request');
+var url = require('url');
+var express = require('express'),
+    app = express();
+
 var router = require('../lib/index.js');
 router(app, {
-    host: 'example.com',
+    host: HOST,
     port: PORT,
     baseDir: __dirname,
-    file: 'routing.json'
+    file: './routing/main_routing.json'
 });
 
-var server = app.listen(PORT, function () {
-    console.log('Express server listening on port ' + PORT);
-});
+/**
+ * @method getAbsoluteURL
+ * @param {String} path
+ * @return {String} Absolute URI to path
+ */
+function getAbsoluteURL(path) {
+    return url.format({
+        protocol: 'http',
+        hostname: HOST,
+        port: PORT,
+        pathname: (path || '')
+    });
+}
 
 describe('checking Status Code of response', function () {
+    beforeEach(function () {
+        this.server = app.listen(PORT, function () {
+            console.log('Express server listening on port ' + PORT);
+        });
+    });
+
     describe('GET non-existent page', function () {
         it('returns status code 404', function (done) {
-            request.get(url.format({
-                protocol: 'http',
-                hostname: HOST,
-                port: PORT
-            }), function(error, response, body) {
-                expect(response.statusCode).toBe(200);
+            request.get(getAbsoluteURL('/non-existent-page/'), function (error, response) {
+                expect(response.statusCode).toBe(404);
                 done();
             });
         });
     });
+
+    afterEach(function () {
+        this.server.close();
+    });
 });
-
-server.close();
-
-// var base_url = 'http://localhost:1337/'
-
-// describe('Hello World Server', function() {
-//   describe('GET /', function() {
-//     it('returns status code 200', function(done) {
-//       request.get(base_url, function(error, response, body) {
-//         expect(response.statusCode).toBe(200);
-//         done();
-//       });
-//     });
-
-//     it('returns Hello World', function(done) {
-//       request.get(base_url, function(error, response, body) {
-//         expect(body).toBe('Hello World');
-//         done();
-//       });
-//     });
-//   });
-// });
