e1ca03521246253ba640e3e945e5563a87ee22ad
Alexander Antonov <alexandr-post@yandex.ru>
2016-01-19 01:54:57 +0300

    new this method should be refactored-it was added

--- spec/request_spec.js
+++ spec/request_spec.js
@@ -44,6 +44,35 @@ describe('checking Status Code of response', function () {
         });
     });
 
+    describe('GET Route without defaults._controller', function () {
+        it('returns Exception', function (done) {
+            request.get(getAbsoluteURL('/route-without-controller/'), function (error, response, body) {
+                expect(response.statusCode).toBe(500);
+                expect(body).toMatch(/Could not load controller &quot;&quot; for route &quot;route_without_controller&quot;./i);
+                done();
+            });
+        });
+    });
+
+    xdescribe('GET Route without defaults._template and defaults._format', function () {
+        it('returns json', function (done) {
+            request.get(getAbsoluteURL('/route-without-template-and-format/'), function (error, response, body) {
+                expect(response.statusCode).toBe(200);
+                expect(body).toBe({});
+                done();
+            });
+        });
+    });
+
+    xdescribe('GET URI allowing only POST method', function () {
+        it('returns status code 405', function (done) {
+            request.get(getAbsoluteURL('/only-post-method-allowing/'), function (error, response) {
+                expect(response.statusCode).toBe(405);
+                done();
+            });
+        });
+    });
+
     afterEach(function () {
         this.server.close();
     });
