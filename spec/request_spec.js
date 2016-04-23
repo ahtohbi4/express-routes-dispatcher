19dcfeb607c68eae8610e0e703e64de5e4cc6bf5
Alexander Antonov <alexandr-post@yandex.ru>
2016-04-24 02:50:35 +0300

    tests were updated

--- spec/request_spec.js
+++ spec/request_spec.js
@@ -56,7 +56,7 @@ describe('checking Status Code of response', function () {
         });
     });
 
-    describe('GET non-existent page', function () {
+    describe('Non-existing URL', function () {
         it('returns status code 404', function (done) {
             request.get(getAbsoluteURL('/non-existent-page/'), function (error, response) {
                 expect(response.statusCode).toBe(404);
@@ -65,32 +65,119 @@ describe('checking Status Code of response', function () {
         });
     });
 
-    describe('GET Route without defaults._controller', function () {
-        it('returns Exception', function (done) {
+    describe('Route without requirement attribute \'defaults._controller\'', function () {
+        it('returns status code 500', function (done) {
             request.get(getAbsoluteURL('/route-without-controller/'), function (error, response, body) {
                 expect(response.statusCode).toBe(500);
-                expect(body).toMatch(/Could not load controller &quot;&quot; for route &quot;route_without_controller&quot;./i);
+                // expect(body).toMatch(/Could not load controller &quot;&quot; for route &quot;route_without_controller&quot;./i);
                 done();
             });
         });
     });
 
-    describe('GET Route without defaults._template and defaults._format', function () {
-        it('returns json', function (done) {
-            request.get(getAbsoluteURL('/route-without-template-and-format/'), function (error, response, body) {
+    describe('Simple route with requirement attributes only', function () {
+        it('returns status code 200', function (done) {
+            request.get(getAbsoluteURL('/route-simple/'), function (error, response) {
                 expect(response.statusCode).toBe(200);
-                expect(body).toBe({});
+                done();
+            });
+        });
+
+        it('returns JSON', function (done) {
+            request.get(getAbsoluteURL('/route-simple/'), function (error, response, body) {
+                expect(body).toMatch({});
                 done();
             });
         });
     });
 
-    describe('GET URI allowing only POST method', function () {
-        it('returns status code 405', function (done) {
+    describe('Route for HTML but without template', function () {
+        it('returns status code 500', function (done) {
+            request.get(getAbsoluteURL('/route-for-html-without-template/'), function (error, response, body) {
+                expect(response.statusCode).toBe(500);
+                // expect(body).toMatch(/Template is not defined./i);
+                done();
+            });
+        });
+    });
+
+    describe('Route with template', function () {
+        it('returns status code 200', function (done) {
+            request.get(getAbsoluteURL('/route-with-template-without-format/'), function (error, response) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it('returns JSON', function (done) {
+            request.get(getAbsoluteURL('/route-with-template-without-format/'), function (error, response, body) {
+                expect(body).toMatch('Hello, World!');
+                done();
+            });
+        });
+    });
+
+    describe('Route for JSON with template', function () {
+        it('returns status code 200', function (done) {
+            request.get(getAbsoluteURL('/route-with-template-for-json/'), function (error, response) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it('returns JSON', function (done) {
+            request.get(getAbsoluteURL('/route-with-template-for-json/'), function (error, response) {
+                expect(response.headers['content-type']).toMatch('application/json');
+                done();
+            });
+        });
+
+        it('returns JSON', function (done) {
+            request.get(getAbsoluteURL('/route-with-template-for-json/'), function (error, response, body) {
+                expect(body).toMatch({});
+                done();
+            });
+        });
+    });
+
+    describe('Route with param', function () {
+        it('returns status code 404 for not matched \'val3\'', function (done) {
+            request.get(getAbsoluteURL('/route-with-param/val3/'), function (error, response) {
+                expect(response.statusCode).toBe(404);
+                done();
+            });
+        });
+
+        it('returns status code 200 for matched \'val1\'', function (done) {
+            request.get(getAbsoluteURL('/route-with-param/val1/'), function (error, response) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+
+        it('returns status code 200 for matched \'val2\'', function (done) {
+            request.get(getAbsoluteURL('/route-with-param/val2/'), function (error, response) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
+
+    describe('Route Only POST method is allowed', function () {
+        it('returns status code 405 for GET', function (done) {
             request.get(getAbsoluteURL('/only-post-method-allowing/'), function (error, response) {
                 expect(response.statusCode).toBe(405);
                 done();
             });
         });
+
+        it('returns status code 200 for POST', function (done) {
+            request.get(getAbsoluteURL('/only-post-method-allowing/'), function (error, response) {
+                expect(response.statusCode).toBe(200);
+                done();
+            });
+        });
     });
+
+    // @todo: add tests for external routes
 });
